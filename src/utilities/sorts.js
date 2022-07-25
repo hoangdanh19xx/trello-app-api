849d8f7d9cb7a53a0997fe599389bd0f63eef395
hoangdanh <hoangdanh19xx@gmail.com>
2022-07-25 17:21:34 +0700

    config babel.js

--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -1,6 +1,4 @@
 // Order an array of objects based on another array order
-const mapOrder = (array, order, key) => {
+export const mapOrder = (array, order, key) => {
   return array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
 };
-
-export { mapOrder };

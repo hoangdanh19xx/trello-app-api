4175b21e54c5358446d0e05b057d1ffef6ccc2bc
hoangdanh <hoangdanh19xx@gmail.com>
2022-07-26 18:44:03 +0700

    CRUD Columns & cards

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,9 +1,11 @@
 import { ColumnModel } from "*/models/Column.model";
 import { BoardModel } from "*/models/board.model";
+import { CardModel } from "*/models/Card.model";
 
 const createNew = async (data) => {
   try {
     const newColumn = await ColumnModel.createNew(data);
+    newColumn.cards = [];
 
     // update columnOrder Array in board collection
     await BoardModel.pushColumnOrder(
@@ -23,9 +25,19 @@ const update = async (id, data) => {
       ...data,
       updatedAt: Date.now(),
     };
-    const result = await ColumnModel.update(id, updateData);
-    return result;
+    if (updateData._id) delete updateData._id;
+    if (updateData._id) delete updateData.cards;
+
+    const updatedColumn = await ColumnModel.update(id, updateData);
+
+    if (updatedColumn._destroy) {
+      // delete many cards in this column
+      CardModel.deleteMany(updatedColumn.cardOrder);
+    }
+
+    return updatedColumn;
   } catch (error) {
+    console.log(error);
     throw new Error(error);
   }
 };
